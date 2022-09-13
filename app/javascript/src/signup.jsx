7593f3e39e19956f88650107e1073abf268175ad
jones89-dan <jones.89.dan@gmail.com>
2022-09-12 21:39:50 -0700

    Added signup form

--- app/javascript/src/signup.jsx
+++ app/javascript/src/signup.jsx
@@ -10,6 +10,25 @@ const Signup = props => (
 
     <Layout>
     <h1>Signup</h1>
+    <div className="login-signup col-6">
+            <div className="float-right sign-up text-white">
+              <form className="p-2 rounded">
+                <div className="new-to-t">
+                  <p className="text-white pt-3"><span> Sign Up</span></p>
+                </div>
+                <div className="form-group">
+                  <input type="text" className="form-control username" placeholder="Username"/>
+                </div>
+                <div className="form-group">
+                  <input type="email"className="form-control email" placeholder="Email"/>
+                </div>
+                <div className="form-group">
+                  <input type="password" className="form-control password" placeholder="Password"/>
+                </div>
+                <button type="submit" id="sign-up-btn" className="btn btn-default btn-warning pull-right">Sign up</button>
+              </form>
+            </div>
+          </div>
     </Layout>
 
 )

9feb2ea403bde93378da03a8b5da8e686ec0437e
jones89-dan <jones.89.dan@gmail.com>
2022-09-23 12:49:00 -0700

    Added Hero image to home page

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -8,6 +8,20 @@ const Home = props => (
 
     <Layout>
     <h1>Home</h1>
+    <div id="hero" class="container-fluid">
+      <div class="container-fluid">
+        <div className="row">
+          <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mx-auto text-center">
+            <div className="hero-text">
+              <div className="col p-2 d-inline-block text-white">
+                <div className="glass w-auto pt-5 pb-5">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
 
 )

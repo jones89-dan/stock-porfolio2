30da6cfabcc2bcd13f388b064012ac29bbfec386
jones89-dan <jones.89.dan@gmail.com>
2022-09-26 12:50:25 -0700

    Working on search form/functin

--- app/javascript/src/search.jsx
+++ app/javascript/src/search.jsx
@@ -4,13 +4,41 @@ import './home.scss';
 import Layout from './layout';
 
 
-const Search = props => (
+class Search extends React.Component {
+  state = {
+    symbol: '',
+    error: '',
+  }
 
-    <Layout>
-    <h1>Search</h1>
-    </Layout>
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
 
-)
+  searchSymbol = (e) => {
+    if (e) { e.preventDefault(); }
+    this.setState({
+      error: '',
+    });
+
+  }
+
+  render () {
+    const { symbol, error } = this.state;
+    return (
+
+      <Layout>
+      <h1>Search</h1>
+        <form onSubmit={this.searchSymbol}>
+            <input name="search" type="text" className="form-control form-control-lg mb-3" placeholder="Search" value={this.symbol} onChange={this.handleChange} required />
+            <button type="submit" className="btn btn-danger btn-block btn-lg">Search</button>
+            {error && <p className="text-danger mt-2">{error}</p>}
+        </form>
+      </Layout>
+    )
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(

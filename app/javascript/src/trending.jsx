135aec3d2c9a69dd24d3dd5caf21cc575ba19524
jones89-dan <jones.89.dan@gmail.com>
2022-09-28 22:35:14 -0700

    Changed to useEffect for setting state, working on mapping response

--- app/javascript/src/trending.jsx
+++ app/javascript/src/trending.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import Layout from './layout';
 import './home.scss';
@@ -7,48 +7,51 @@ import { getSymbolData } from 'src/utils/requests'
 //require 'dotenv/load'
 
 
-class Trending extends React.Component {
-  state = {
-    stock: {},
-    symbol: '',
-    data: {},
-    currentPrice: '',
-    error: '',
-  }
+const Trending = () => {
 
-  componentDidMount() {
-    const encodedParams = new URLSearchParams();
-    encodedParams.append("symbol", "AAPL");
+    const [stockData, setStockData] = useState([])
+    const [response, setResponse] = useState([])
+    const [symbol, setSybmol] = useState([])
+    const [data, setData] = useState([])
+    const [currentPrice, setCurrentPrice] = useState([])
+    const [error, setError] = useState([])
 
-    const options = {
-    	method: 'POST',
-    	headers: {
-    		'content-type': 'application/x-www-form-urlencoded',
-    		'X-RapidAPI-Key': process.env.X_RapidAPI_Key,
-    		'X-RapidAPI-Host': 'yahoo-finance97.p.rapidapi.com'
-    	},
-    	body: encodedParams
-    };
+    const allData = function (response) {
+    setStockData(response.stockData.map(data => data));
+  };
 
-    fetch('https://yahoo-finance97.p.rapidapi.com/stock-info', options)
-  	 .then(response => response.json())
-  	 .then(response => console.log(response))
-     .then(response => {
-       console.log(response.data.currentPrice)
-       this.setState({
-         stock: response,
-        })
-      })
-      .catch(err => console.error(err));
-    }
+    useEffect(() => {
+      const encodedParams = new URLSearchParams();
+      encodedParams.append("symbol", "AAPL");
 
-  render () {
+      const options = {
+      	method: 'POST',
+      	headers: {
+      		'content-type': 'application/x-www-form-urlencoded',
+      		'X-RapidAPI-Key': process.env.X_RapidAPI_Key,
+      		'X-RapidAPI-Host': 'yahoo-finance97.p.rapidapi.com'
+      	},
+      	body: encodedParams
+      };
+
+      fetch('https://yahoo-finance97.p.rapidapi.com/stock-info', options)
+    	 .then(response => response.json())
+    	 .then(response => console.log(response.data.currentPrice))
+       .then(response => setStockData(response))
+       .catch(err => console.error(err));
+    }, []);
+
     return (
       <Layout>
       <h1>Trending</h1>
+          {stockData && stockData.map(data => {
+            return (
+              <div key={data.data}>
+                <p>{data.data}</p>
+              </div> )
+            })}
       </Layout>
     )
-  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {

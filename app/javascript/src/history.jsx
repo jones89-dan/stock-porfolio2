7428598c90680bd15f2289e8e602a4bd5f25f2b7
jones89-dan <jones.89.dan@gmail.com>
2022-11-01 21:17:01 -0700

    graphing opening/closing price working

--- app/javascript/src/history.jsx
+++ app/javascript/src/history.jsx
@@ -57,33 +57,25 @@ const History = () => {
     console.log(time);
   }
 
-  const createChart = function (data) {
+  const createChart = function (openData, closeData) {
 
-    console.log(data)
+    console.log(openData)
     const ctx = document.getElementById('myChart').getContext('2d');
 
     const myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: ['Red'],
             datasets: [{
                 label: 'AAPL',
-                data: [data],
+                threshold: closeData,
+                data: [[openData, closeData]],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(255, 99, 132, 1)',
+
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
                 ],
                 borderWidth: 1
             }]
@@ -104,7 +96,7 @@ const History = () => {
     <h1>Histroy</h1>
     {apiResponse.map(info => {
       formatDate(info.Date);
-      createChart(info.Open);
+      createChart(info.Open, info.Close);
     })}
     <canvas id="myChart" style={{width:"400", height:"400"}}></canvas>
     </Layout>

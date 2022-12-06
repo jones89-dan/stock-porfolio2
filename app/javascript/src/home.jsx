a766349c145c50b9b2ab150a98acee654402962f
jones89-dan <jones.89.dan@gmail.com>
2022-12-05 20:40:07 -0800

    Continued work on typing animation

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -5,20 +5,34 @@ import Layout from './layout';
 
 const Home = () => {
 
+
+
+  const textList = [
+    "Welcome to the financial stock app.",
+    "See stock data in real time.",
+    "View historical trends and track stocks you have invested in.."
+  ];
+
   const [text, setText] = useState("")
-  const [fullText, setFullText] = useState(
-    "test."
-  )
+  const [fullText, setFullText] = useState(textList[0])
   const [index, setIndex] = useState(0)
+
+  let l = textList.length;
 
   useEffect(() => {
     if (index < fullText.length) {
       setTimeout(() => {
         setText(text + fullText[index])
         setIndex(index + 1)
-      }, 400)
+      }, 100);
+    } else {
+      setTimeout(() => {
+        setFullText(textList[Math.floor(Math.random() * l)]);
+        setText("");
+        setIndex(0)
+      }, 2000);
     }
-  }, [index])
+  }, [index]);
 
   return (
     <Layout>
@@ -30,9 +44,19 @@ const Home = () => {
                 <div className="col p-2 d-inline-block text-white">
                   <div className="glass w-auto p-5">
                    <h1>Stock Portfolio</h1>
-                   <h2>{text}</h2>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid pt-3 bg-dark">
+        <div className="row">
+          <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mx-auto text-center">
+            <div className="col p-2 d-inline-block text-white">
+              <div className="w-auto p-5">
+                <p>{text}</p>
               </div>
             </div>
           </div>

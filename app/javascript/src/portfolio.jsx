f810bcf5496cdc6f2739e123c21b897eb1042f4f
jones89-dan <jones.89.dan@gmail.com>
2022-12-07 21:06:48 -0800

    Added table to portfolio page

--- app/javascript/src/portfolio.jsx
+++ app/javascript/src/portfolio.jsx
@@ -8,6 +8,22 @@ const Portfolio = props => (
     <Layout>
       <div className="text-white">
         <h1>Portfolio</h1>
+          <div className="p-5">
+                <table className="table table-dark">
+                  <thead>
+                    <tr>
+                      <th scope="col">Symbol</th>
+                      <th scope="col">Current Price</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                        <tr>
+                          <th scope="row"><a href={'./history?'}></a></th>
+                          <td scope="col"></td>
+                        </tr>
+                  </tbody>
+                </table>
+          </div>
       </div>
     </Layout>
 

520496e61702f297e075a97f0faadb765ed1c069
jones89-dan <jones.89.dan@gmail.com>
2022-12-09 12:51:54 -0800

    Working on formatting/styling sign up page

--- app/javascript/src/signup.jsx
+++ app/javascript/src/signup.jsx
@@ -9,26 +9,28 @@ import Layout from './layout';
 const Signup = props => (
 
     <Layout>
+    <div className="text-white">
     <h1>Signup</h1>
-    <div className="login-signup col-6">
-            <div className="float-right sign-up text-white">
-              <form className="p-2 rounded">
-                <div className="new-to-t">
-                  <p className="text-white pt-3"><span> Sign Up</span></p>
-                </div>
-                <div className="form-group">
-                  <input type="text" className="form-control username" placeholder="Username"/>
-                </div>
-                <div className="form-group">
-                  <input type="email"className="form-control email" placeholder="Email"/>
-                </div>
-                <div className="form-group">
-                  <input type="password" className="form-control password" placeholder="Password"/>
-                </div>
-                <button type="submit" id="sign-up-btn" className="btn btn-default btn-warning pull-right">Sign up</button>
-              </form>
+      <div className="login-signup col-6">
+        <div className="float-right sign-up text-white">
+          <form className="p-2 rounded">
+            <div className="new-to-t">
+              <p className="text-white pt-3"><span> Sign Up</span></p>
             </div>
+            <div className="form-group">
+              <input type="text" className="form-control username" placeholder="Username"/>
+            </div>
+            <div className="form-group">
+              <input type="email"className="form-control email" placeholder="Email"/>
+            </div>
+            <div className="form-group">
+              <input type="password" className="form-control password" placeholder="Password"/>
+            </div>
+              <button type="submit" id="sign-up-btn" className="btn btn-default btn-danger pull-right">Sign up</button>
+            </form>
           </div>
+        </div>
+      </div>
     </Layout>
 
 )

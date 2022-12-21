85a7404741974bf85816b85dea3f5cd5c42f10ef
jones89-dan <jones.89.dan@gmail.com>
2022-12-21 13:12:10 -0800

    Working on trending page table styles

--- app/javascript/src/trending.jsx
+++ app/javascript/src/trending.jsx
@@ -30,8 +30,16 @@ const Trending = () => {
         try {
           const response = await fetch('https://yahoo-finance97.p.rapidapi.com/stock-info', options);
           const json = await response.json();
-          console.log(json);
+          console.log(json.data);
           setTrending(trendingArr => [...trendingArr, json.data]);
+
+          for (let i = 0; i <= json.data.length; i++) {
+              if (!json.data[i].hasOwnProperty('earningsGrowth')) {
+                continue;
+              }
+
+              console.log('true')
+          }
 
         } catch (error) {
             console.log("error", error);
@@ -50,7 +58,7 @@ const Trending = () => {
         <div className="text-white">
           <h1>Trending</h1>
                 <div className="p-5">
-                      <table className="table table-dark">
+                      <table className="table table-dark table-hover rounded">
                         <thead>
                           <tr>
                             <th scope="col">Symbol</th>
@@ -63,8 +71,8 @@ const Trending = () => {
                             return (
                               <tr>
                                 <th scope="row" key={data.symbol}><a href={'./history?' + data.symbol}>{data.symbol}</a></th>
-                                <td scope="col">${data.currentPrice}</td>
-                                <td scope="col" id="eGrowth">${data.earningsGrowth}</td>
+                                <td scope="col">${data.currentPrice.toFixed(2)}</td>
+                                <td scope="col" id="eGrowth">${data.earningsGrowth.toFixed(2)}</td>
                                 {/*data.earningsGrowth < 0 ? document.getElementById("eGrowth").className="red" : console.log("positve")*/}
                               </tr>
                             )

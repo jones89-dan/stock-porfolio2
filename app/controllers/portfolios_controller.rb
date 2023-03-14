a88bdf3aa1870c0fb97e40c85a8ac3fa99e0c8f5
jones89-dan <jones.89.dan@gmail.com>
2023-03-14 13:12:01 -0700

    Working on portfolio controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,18 @@
 class PortfoliosController < ApplicationController
+  def add
+      token = cookies.signed[:stock_session_token]
+      session = Session.find_by(token: token)
+      user = session.user
+      @symbol = user.portfolios.new(portfolio_params)
+
+      if @symbol.save
+        render ''
+      end
+    end
+
+private
+  def portfolio_params
+       params.require(:portfolio).permit(:symbol)
+  end
+
 end
